2a81fd71dea369bc19101939a05d00f41934784d
mlnvgl <vogel.melanie@outlook.com>
2026-01-06 16:10:18 +0100

    Build the library.

--- src/lib.zig
+++ src/lib.zig
@@ -1,10 +1,10 @@
-const sdt = @import("std");
+const std = @import("std");
 
 pub export fn add(a: i32, b: i32) i32 {
     return a + b;
 }
 
 pub export fn hello() void {
-    const stdout = sdt.io.getStdOut().writer();
-    _ = stdout.print("Hello from zour_dough\n", .{});
+    // Use std.debug.print for a simple, stable output path
+    std.debug.print("Hello from zour_dough\n", .{});
 }
