cc3be2cf0631d7df0b969c25db394f3702359a72
mlnvgl <vogel.melanie@outlook.com>
2026-02-15 17:10:25 +0100

    Python setup works.

--- micro-python/zour-dough-ds18b20.py
+++ micro-python/zour-dough-ds18b20.py
@@ -2,9 +2,17 @@ from machine import Pin
 import onewire
 import ds18x20
 import time
-
+import ubinascii
 
 data_pin = Pin(22)
+heater_pin = Pin(21, Pin.OUT)
+heater_pin.value(0)
+
+
+MAX_TEMP = 25  # Celsius - turn heater off
+MIN_TEMP = 23  # Celsius - turn heater on
+CHECK_INTERVAL = 3  # Seconds between temperature checks
+
 ow = onewire.OneWire(data_pin)
 ds = ds18x20.DS18X20(ow)
 
@@ -15,11 +23,24 @@ else:
 	print("Found {} sensor(s)".format(len(roms)))
 
 while True:
-	ds.convert_temp()
-	time.sleep_ms(750)
+	try:
+		ds.convert_temp()
+		time.sleep_ms(750)
 
-	for rom in roms:
-		temp_c = ds.read_temp(rom)
-		print("DS18B20 {}: {:.2f} C".format(rom, temp_c))
+		for rom in roms:
+			temp_c = ds.read_temp(rom)
+			print("DS18B20 {}: {:.2f} C".format(ubinascii.hexlify(rom).decode(), temp_c))
+			if temp_c >= MAX_TEMP:
+				heater_pin.value(0)  # Heater OFF
+				print("Heater OFF")
+			elif temp_c <= MIN_TEMP:
+				heater_pin.value(1)  # Heater ON
+				print("Heater ON")
 
-	time.sleep(1)
+			print("PerfectCurrent Temperature of {:.2f} C".format(temp_c))
+
+		print("---")
+	except Exception as e:
+		print("Error: {}".format(e))
+		
+	time.sleep(CHECK_INTERVAL)
